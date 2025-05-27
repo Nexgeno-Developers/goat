4c95c9f754a77e6044c44c7fb5889d4bc6a91d5b
rashidweb1 <rashid.makent@gmail.com>
2025-05-27 13:04:16 +0530

    email issue

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -103,7 +103,7 @@ class Cronjob extends CI_Controller {
         date_default_timezone_set('Asia/Kolkata');
         $subject = "Goat Movement Report Till Now " . date('d-M-Y h:i:s A');
         //$logoUrl = $this->settings_model->get_logo_light().'?v='.time();
-        $logoUrl = 'https://i.ibb.co/rRj25z1r/logo-light.png';
+        $logoUrl = 'https://i.ibb.co/nsP1fvGN/logo-dark-webp.webp';
         $body = '
 <html>
 <head>
